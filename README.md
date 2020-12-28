9998057e0229c466ec2d93f2f18a12be870a5d2e
Caltrop <caltrop@caltrop.dev>
2020-12-28 15:53:07 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # npm-download-inflator
- Artificially inflate the download count of any npm package
+Artificially increase the download-counter of any npm package!
+
+Read more about this [here](https://caltrop.dev/why-you-should-not-trust-npm-download-count).
+
+## Install
+
+`$ npm install --save download-inflator`
+
+## Usage
+
+Requiring `download-inflator` will return a class. Upon instantiation it will increment the download count of the 
+specified package every second.
+
+```js
+var DownloadInflator = require('download-inflator');
+
+var inflator = new DownloadInflator({
+	name: 'download-inflator'
+});
+```
+
+### Options
+
+#### name
+Type: `string`
+
+The name of the npm package.
+
+#### onPing
+Type: `function`
+
+A function to be called whenever the download count gets increased.
+
+Default: `function() {}`
+
+```js
+var DownloadInflator = require('download-inflator');
+
+var inflator = new DownloadInflator({
+    name: 'download-inflator',
+    onPing: function(timesPinged) {
+		console.log('The download count has been incremented ' + timesPinged + ' times!');
+	}
+});
+```
+
+#### delay
+Type `number`
+
+The amount of time to wait between pings in milliseconds
+
+Default: `1000`
+
+##### version
+Type `string`
+
+The version of the package.
+
+Default: `'1.0.0'`
+
+#### scope
+Type `string`
+
+The scope of the npm package if it is scoped
+
+Default: `null`
+
+```js
+var DownloadInflator = require('download-inflator');
+
+var inflator = new DownloadInflator({
+    name: 'download-inflator',
+    version: '1.0.0',
+	scope: 'caltrop'
+});
+```
+
+### Methods
+
+#### stop
+
+Stops the interval, optionally takes an amount of time in milliseconds to wait before stopping.
+
+The example below increases the download count of this package once every 250 milliseconds and automatically stops after 10 seconds:
+```js
+var DownloadInflator = require('download-inflator');
+
+var inflator = new DownloadInflator({
+    name: 'download-inflator',
+    delay: 250
+});
+
+inflator.stop(10 * 1000);
+```
+
+## License
+This demo is licensed under the GNU General Public License v3.0. You may copy, distribute and modify the software as long as you track changes/dates in source files. Any modifications to this project must be made available under the GPL along with build & install instructions.
---
+++ index.js
@@ -0,0 +1,72 @@
+var {get} = require('https');
+
+function isValid(s) {
+    return typeof s === 'string' && s.length;
+}
+
+/**
+ * @typedef options
+ * @type {Object}
+ * @property {string} name The name of the package
+ * @property {function(number):undefined} [onPing]
+ * @property {number} [delay] Time to wait inbetween pings
+ * @property {string} [version] The version of the package
+ * @property {string} [scope] The name of the scope if required
+ */
+
+/**
+ * @class DownloadInflator
+ * @description Inflates the download count of an npm package
+ * @public
+ * @property {number} timesPinged
+ * @property {function(number):undefined} onPing
+ * @param {options} options 
+ */
+function DownloadInflator(options = {}) {
+    if(!DownloadInflator[Symbol.hasInstance](this)) return new DownloadInflator(options);
+    if(!isValid(options.name)) throw TypeError('Package name must be a String!');
+    var delay = Number(options.delay);
+    if(typeof options.delay !== 'undefined' && (delay !== delay || delay === Infinity || delay <= 0)) throw new TypeError('Ping delay must be a valid finite number above 0');
+    if(!isValid(options.version)) options.version = '1.0.0';
+    if(!isValid(options.scope)) options.scope = '';
+    if(options.scope.charAt(0) === '@') options.scope = options.scope.substring(1);
+    this.$stopped = false;
+    var {name, version, scope} = options;
+    var url = encodeURI('https://registry.npmjs.org/'+(scope.length ? '@'+scope+'/'+ name : name)+'/-/'+name+'-'+version+'.tgz');
+    var self = this;
+    new Promise(function(resolve, reject) {
+        get(url, function(res) {
+            resolve(res.statusCode === 200);
+        }).on('error', reject);
+    }).then(function(packageExists) {
+        if(self.$stopped) return;
+        if(!packageExists) throw new Error('The specified package does not exist or can\'t be reached at the moment!');
+        self.timesPinged = 0;
+        self.onPing = typeof options.onPing === 'function' ? options.onPing : function() {};
+        self.$intervalId = setInterval(get.bind(null, url, function() {
+            self.timesPinged += 1;
+            if(typeof self.onPing === 'function') self.onPing(self.timesPinged);
+        }), delay || 1000);
+    }).catch(console.error);
+};
+
+/**
+ * Ends the pinging delay
+ * @param {number} [delay] Optionally, how long to wait before stopping in milliseconds 
+ * @returns {void}
+ */
+DownloadInflator.prototype.stop = function stop(delay) {
+    var del = Number(delay);
+    if(typeof delay !== 'undefined' && (del !== del || del === Infinity || del <= 0)) throw new TypeError('Stop delay must be a valid finite number above 0');
+    var self = this;
+    function end() {
+        self.$stopped = true;
+        clearInterval(self.$intervalId);
+    }
+    if(del) {
+        setTimeout(end, del);
+    } else {
+        end();
+    }
+};
+module.exports = DownloadInflator;
